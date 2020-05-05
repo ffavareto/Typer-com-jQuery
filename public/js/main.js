492c7bdaac664126c82a80118f5a463183eab0e7
Lucas Santos <lucasfavaretosantos98@gmail.com>
2020-05-04 22:02:58 -0300

    organização do codigo

--- public/js/main.js
+++ public/js/main.js
@@ -65,43 +65,6 @@ function inicializaMarcadores() {
   });
 }
 
-function inserePlacar() {
-  let corpoTabela = $('.placar').find('tbody');
-  let usuario = 'Lucas'
-  let numPalavras = $('#contador-palavras').text();
-
-  let linha = novaLinha(usuario, numPalavras);
-  linha.find('.botao-remover').click(removelinha);
-
-  corpoTabela.prepend(linha);
-}
-
-function novaLinha(usuario, palavras) {
-  let linha = $('<tr>');
-  let colunaUsuario = $('<td>').text(usuario);
-  let colunaPalavras = $('<td>').text(palavras);
-  let colunaRemover = $('<td>');
-
-  let link = $('<a>').attr('href', '#').addClass('botao-remover');
-  let icone = $('<i>').addClass('small').addClass('material-icons').text('delete');
-
-  link.append(icone);
-
-  colunaRemover.append(link);
-
-  linha.append(colunaUsuario);
-  linha.append(colunaPalavras);
-  linha.append(colunaRemover);
-
-  return linha;
-
-}
-
-function removelinha(event) {
-  event.preventDefault();
-  $(this).parent().parent().remove();
-}
-
 function reiniciaJogo() {
   campo.attr('disabled', false);
   campo.val('');
